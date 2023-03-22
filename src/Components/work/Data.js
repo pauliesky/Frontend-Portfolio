b31f4806739590eee4ead1212e2b978df57efff0
Akinleye Paul <paulakinleye@gmail.com>
2023-03-22 18:25:04 +0100

    made some changes

--- src/Components/work/Data.js
+++ src/Components/work/Data.js
@@ -1,5 +1,5 @@
 import Work1 from '../../assets/work1.png'
-// import Work2 from '../../assets/work2.jpg'
+import Work2 from '../../assets/work2.jpg'
 // import Work3 from '../../assets/work3.jpg'
 // import Work4 from '../../assets/work4.jpg'
 import Work5 from '../../assets/work5.png'
@@ -20,36 +20,25 @@ export const projectsData = [
 
 
   },
-  // {
-  //   id: 2,
-  //   image: Work2,
-  //   title: 'App movil',
-  //   category: 'app'
-
-  // },
-  // {
-  //   id: 3,
-  //   image: Work3,
-  //   title: 'Brand design',
-  //   category: 'design'
-
-  // },
-  // {
-  //   id: 4,
-  //   image: Work4,
-  //   title: 'App Movil',
-  //   category: 'app'
-
-  // },
   {
     id: 5,
     image: Work5,
     title: 'Photography Site',
-    about: 'A professional catalogue of a lensman',
+    about: 'A professional catalogue of a lens man',
     stack: 'React,Hooks,CSS,emailJS',
     role: 'Sole Engineer',
     category: 'web',
     site: 'https://cleath.netlify.app/'
+  },
+  {
+    id: 2,
+    image: Work2,
+    title: 'Financial Technology',
+    about: 'E-shop for all matters relating to loans',
+    stack: 'HTML,CSS,JavScript,React',
+    role: 'Frontend Engineer',
+    category: 'web',
+    site: 'https://credbevy.netlify.app/'
   },
 ];
 
